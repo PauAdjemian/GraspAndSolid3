f9b9bd9af0a094e4dad7d9b14a5fd97408fd1b3e
PauAdjemian <paulaadjemian2004@gmail.com>
2023-05-08 20:21:12 -0300

    cambios

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -8,4 +8,5 @@ public interface IPrinter
 Se crea la interface IPrinter que va a ser implementada en este momento por: ConsolePrinter y FilePrinter.
 Cumplimos con SRP ya que cada clase tiene una sola responsabilidad y Expert porque conoce lo suficiente.
 Cumplimos con el patrón de Polimorfismo porque eliminamos la pregunta por el destino.
+Si el día de mañana queres imprimir de otra forma, no es necesario tocar el resto de las clases.  
 */
